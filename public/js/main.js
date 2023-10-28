eae7bd7109badfe4c14b3f8e16349142d9263df8
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-10-28 14:05:49 -0300

    Refactoring JavaScript

--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,44 @@
 const username = document.querySelector("#username");
 const regUserBtn = document.querySelector("#regUserBtn");
+const socket = io();
+const form = document.querySelector("form");
+const messagesDiv = document.querySelector("#messagesDiv");
+const messages = document.getElementById("messages");
+const input = document.querySelector("#input");
+const submit = document.querySelector("#submit");
+const userID = crypto.randomUUID();
 let user;
-let userID;
 
 const saveUser = () => {
   user = username.value;
-  userID = crypto.randomUUID();
   document.querySelector("#registatrion").classList.add("hidden");
   document.querySelector("#chatMessage").classList.remove("hidden");
 };
 
 document.addEventListener("keydown", function (event) {
-  if (event.key === "Enter" || event.keyCode === 13) {
-    saveUser();
-  }
+  (event.key === "Enter" || event.keyCode === 13) && username.value !== ""
+    ? saveUser()
+    : null;
 });
 
 regUserBtn.addEventListener("click", saveUser);
 
-username.addEventListener("input", () => {
-  if (username.value != "") {
-    regUserBtn.disabled = false;
-    regUserBtn.classList.remove("bg-gray-500");
-    regUserBtn.classList.add("bg-green-500");
-    regUserBtn.classList.add("hover:bg-green-700");
+function changeBtn(btn, inp) {
+  if (inp.value != "") {
+    btn.disabled = false;
+    btn.classList.remove("bg-gray-500");
+    btn.classList.add("bg-green-500");
+    btn.classList.add("hover:bg-green-700");
   } else {
-    regUserBtn.disabled = true;
-    regUserBtn.classList.remove("bg-green-500");
-    regUserBtn.classList.add("bg-gray-500");
-    regUserBtn.classList.remove("hover:bg-green-700");
+    btn.disabled = true;
+    btn.classList.remove("bg-green-500");
+    btn.classList.add("bg-gray-500");
+    btn.classList.remove("hover:bg-green-700");
   }
-});
+}
 
-const input = document.querySelector("#input");
-const submit = document.querySelector("#submit");
-
-input.addEventListener("input", () => {
-  if (input.value != "") {
-    submit.disabled = false;
-    submit.classList.remove("bg-gray-500");
-    submit.classList.add("bg-green-500");
-    submit.classList.add("hover:bg-green-700");
-  } else {
-    submit.disabled = true;
-    submit.classList.remove("bg-green-500");
-    submit.classList.add("bg-gray-500");
-    submit.classList.remove("hover:bg-green-700");
-  }
-});
-
-const socket = io();
-const form = document.querySelector("form");
-const messages = document.getElementById("messages");
-const messagesDiv = document.querySelector("#messagesDiv");
+input.addEventListener("input", changeBtn.bind(null, submit, input));
+username.addEventListener("input", changeBtn.bind(null, regUserBtn, username));
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -72,7 +58,6 @@ socket.on("chat message", (msg) => {
       <p class="text-red-800 font-semibold">${msg[0]}:</p>
       <p class="text-gray-900">${msg[1]}</p>
     </div>
-
   `;
   } else {
     item.innerHTML = `
