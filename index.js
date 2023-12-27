7a6874f8f89f4f249f43a5f1dfd133b4a8295239
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-12-27 11:09:39 -0300

    refactoring js frontend file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const PORT = process.env.PORT | 8000;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,10 +12,8 @@ io.on('connection', (socket) => {
   socket.on('chat message', (message) => {
     io.emit('chat message', message);
   });
-
 });
 
-const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log(`Server on: http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-const username = document.querySelector("#username");
+const usernameInput = document.querySelector("#username");
 const regUserBtn = document.querySelector("#regUserBtn");
 const socket = io();
 const form = document.querySelector("form");
@@ -10,63 +10,54 @@ const userID = crypto.randomUUID();
 let user;
 
 const saveUser = () => {
-  user = username.value;
+  user = usernameInput.value;
   document.querySelector("#registatrion").classList.add("hidden");
   document.querySelector("#chatMessage").classList.remove("hidden");
 };
 
-document.addEventListener("keydown", function (event) {
-  (event.key === "Enter" || event.keyCode === 13) && username.value !== ""
-    ? saveUser()
-    : null;
+document.addEventListener("keydown", (event) => {
+  if ((event.key === "Enter" || event.keyCode === 13) && usernameInput.value !== "") {
+    saveUser();
+  }
 });
 
 regUserBtn.addEventListener("click", saveUser);
 
-function changeBtn(btn, inp) {
-  if (inp.value != "") {
+function toggleButtonState(btn, inp) {
+  if (inp.value !== "") {
     btn.disabled = false;
     btn.classList.remove("bg-gray-500");
-    btn.classList.add("bg-green-500");
-    btn.classList.add("hover:bg-green-700");
+    btn.classList.add("bg-green-500", "hover:bg-green-700");
   } else {
     btn.disabled = true;
-    btn.classList.remove("bg-green-500");
+    btn.classList.remove("bg-green-500", "hover:bg-green-700");
     btn.classList.add("bg-gray-500");
-    btn.classList.remove("hover:bg-green-700");
   }
 }
 
-input.addEventListener("input", changeBtn.bind(null, submit, input));
-username.addEventListener("input", changeBtn.bind(null, regUserBtn, username));
+input.addEventListener("input", () => toggleButtonState(submit, input));
+usernameInput.addEventListener("input", () => toggleButtonState(regUserBtn, usernameInput));
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   socket.emit("chat message", [user, input.value, userID]);
   input.value = "";
-  submit.disabled = true;
-  submit.classList.remove("bg-green-500");
-  submit.classList.remove("hover:bg-green-700");
-  submit.classList.add("bg-gray-500");
+  toggleButtonState(submit, input);
 });
 
 socket.on("chat message", (msg) => {
   const item = document.createElement("li");
-  if (msg[0] === user && userID === msg[2]) {
-    item.innerHTML = `
-    <div class="mb-4 bg-green-500 p-2 w-4/5 lg:w-3/5 rounded-md float-right">
-      <p class="text-red-800 font-semibold">${msg[0]}:</p>
+  const userMatches = msg[0] === user && userID === msg[2];
+  const bgColorClass = userMatches ? "bg-green-500" : "bg-gray-400";
+  const floatClass = userMatches ? "float-right" : "float-left";
+
+  item.innerHTML = `
+    <div class="mb-4 ${bgColorClass} p-2 w-4/5 lg:w-3/5 rounded-md ${floatClass}">
+      <p class="${userMatches ? 'text-red-800' : 'text-blue-800'} font-semibold">${msg[0]}:</p>
       <p class="text-gray-900">${msg[1]}</p>
     </div>
   `;
-  } else {
-    item.innerHTML = `
-    <div class="mb-4 bg-gray-400 p-2 w-4/5 lg:w-3/5 rounded-md float-left">
-        <p class="text-blue-800 font-semibold">${msg[0]}:</p>
-        <p class="text-gray-900">${msg[1]}</p>
-    </div>
-  `;
-  }
+
   messages.appendChild(item);
   messagesDiv.scrollTo(0, messagesDiv.scrollHeight);
 });
